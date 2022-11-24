d3abb8e588e441745ec31a4f8dc39de97f3fc9bc
Kieu Anh Duc <kieuanhduc2883@gmail.com>
2022-11-24 13:41:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+$(window).scroll(function () {
+if ($(this).scrollTop()) {
+    $(".btn__scroll").fadeIn();
+} else {
+    $(".btn__scroll").fadeOut();
+}
+});
+$(".btn__scroll").click(function () {
+$("html, body").animate({ scrollTop: 0 }, 200);
+});
+});
